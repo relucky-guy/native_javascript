ecb8fa881ab7f1d458a5dcb2f36a9f3e76ae7b1d
Владислав Банников <vlad@bannikon.ru>
2022-09-26 23:44:06 +0300

    I'm doing 6 exercise

--- script.js
+++ script.js
@@ -256,38 +256,182 @@
 
 4) Потренироваться и переписать цикл еще двумя способами*/
 
-const number0fFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+// const number0fFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 
-const personalMovieDB = {
-    count: number0fFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: number0fFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
 
-    if(a != null && b!= null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
+//     if(a != null && b!= null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
     
+// }
+
+// if (personalMovieDB.count > 30) {
+//     prompt('Вы киноман');
+// } else if (personalMovieDB.count > 10) {
+//     prompt('Вы классический зритель');
+// } else if (personalMovieDB.count >= 0) {
+//     prompt('Просмотрено довольно мало фильмов');
+// } else {
+//     prompt('Произошла ошибка');
+// }
+
+// console.log(personalMovieDB);
+
+//25 lesson
+
+// function showFirstMessage() {
+//     console.log('Hello world!');
+// }
+
+// showFirstMessage();
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage('Hello World!');
+// console.log(num);
+
+// function calc(a, b) {
+//     return(a + b);
+// }
+
+// console.log(calc(15, 110));
+// console.log(calc(35, 10));
+// console.log(calc(11, 12));
+
+// function ret() {
+//     let num = 50
+
+//     //
+
+//     return(num);
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log('Hello')
+// };
+
+// logger();
+
+// const calc = (a, b) => { return a + b };
+
+// 26 lesson
+
+/* const usdCurr = 28;
+const eurCurr = 32;
+
+function convert(amount, curr) {
+    console.log(curr * amount);
 }
 
-if (personalMovieDB.count > 30) {
-    prompt('Вы киноман');
-} else if (personalMovieDB.count > 10) {
-    prompt('Вы классический зритель');
-} else if (personalMovieDB.count >= 0) {
-    prompt('Просмотрено довольно мало фильмов');
-} else {
-    prompt('Произошла ошибка');
+convert(500, usdCurr);
+convert(500, eurCurr); */
+
+// 27 lesson
+
+/* const usdCurr = 28;
+const eurCurr = 32;
+const discout = 0.9;
+
+function convert(amount, curr) {
+    return curr * amount;
 }
 
-console.log(personalMovieDB);
+function promotion(result) {
+    console.log(result * discout);
+}
+
+const res = convert(500, usdCurr);
+promotion(res);
+
+function test() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        if (i === 3) return
+    }
+    console.log('Done');
+}
+
+test();
+
+function doNothing() {}
+
+console.log(doNothing() === undefined); */
+
+//4 code writing exercise 
+
+/* Задачи:
+
+1) Создайте функцию, которая будет на вход принимать 1 аргумент с именем человека и возвращать строку.
+
+Пример: вызов функции sayHello('Антон') возвращает строку "Привет, Антон!". В решении вызывать функцию не нужно, программа сделает это за вас.
+
+P.S. возвращать - это использовать ключевое слово return.
+
+P.S.S. Это классическая функция-модификатор, которых мы дальше по курсу будем создавать еще много в разных видах.
+
+2) Создайте функцию, которая принимает в себя 1 аргумент в виде целого числа и возвращает массив из трех чисел: одно на 1 меньше, сам аргумент, и число на 1 больше.
+
+Пример: вызов функции returnNeighboringNumbers(5) возвращает массив в виде [4, 5, 6].
+
+3) Создайте функцию, которая будет принимать в себя 2 аргумента, оба числа. Первое число - это база, второе число - это сколько раз нужно будет повторить это число в прогрессии. (Смотри пример ниже). Функция должна возвращать строку (или число в особых случаях, о которых ниже), где эти числа идут по порядку, разделенные тремя дефисами "---". После последнего числа их не должно быть.
+
+Если второй аргумент не является числом, равен или меньше нуля - то возвращать просто первый аргумент. (Проверяем через оператор typeof)
+
+Примеры:
+
+Вызов функции getMathResult(5, 3) даст ответ 5---10---15
+
+Вызов функции getMathResult(3, 10) даст ответ 3---6---9---12---15---18---21---24---27---30
+
+Вызов функции getMathResult(10, 5) даст ответ 10---20---30---40---50
+
+Вызов функции getMathResult(10, '5') даст ответ 10
+
+Вызов функции getMathResult(10, 0) даст ответ 10
+
+Вызов функции getMathResult(20, -5) даст ответ 20
+
+Эта задача уже ближе к реальности, когда вам нужно учитывать и тип данных у аргументов, проверять их и продумывать логику работы внутри. Обратите внимание на прогрессию, она рассчитывается по простой формуле умножения. Если первый аргумент 5, а второй 3, то число повторяется 3 раза, каждый раз увеличиваясь на само себя. Это базовая математика, которая и нужна для работы в 95% случае на фронтенде.
+
+Да, задача сложнее, но она просто объединяет все то, что мы уже учили. */
+
+// Место для первой задачи
+function sayHello(name) {
+    return `Привет, ${name}!`;
+}
+
+console.log(sayHello('Антон'));
+
+// Место для второй задачи
+function returnNeighboringNumbers() {
+
+}
+
+// Место для третьей задачи
+function getMathResult() {
+
+}
