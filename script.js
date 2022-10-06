a61587551c41545baf1cdc90a7acabcf87215efa
Владислав Банников <vlad@bannikon.ru>
2022-10-06 18:17:49 +0300

    added 12 exercise

--- script.js
+++ script.js
@@ -1162,12 +1162,30 @@ RUB
 const someString = 'This is some strange string';
 
 function reverse(str) {
-
+    if (typeof(str) !== 'string') {
+        return 'Ошибка!';
+    }
+    str = str.split("").reverse().join("");
+    return str;
 }
+
+console.log(reverse(someString));
 
 const baseCurrencies = ['USD', 'EUR'];
 const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
 function availableCurr(arr, missingCurr) {
+    let str = '';
 
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+
+    arr.forEach(function(curr, i) {
+        if (curr !== missingCurr) {
+            str +=`${curr}\n`;
+        }
+    });
+
+    return str;
 }
+
+availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY');
